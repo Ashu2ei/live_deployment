66fa5c4a8fe9a44a66416e2d658e397b586580c4
ashutosh <ashutoshmishra333@gmail.com>
2022-02-07 23:05:41 +0530

    second commit

--- live_deployment/settings.py
+++ live_deployment/settings.py
@@ -27,16 +27,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'authentication.User'
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -74,12 +76,15 @@ WSGI_APPLICATION = 'live_deployment.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'ashutosh',                      
+            'USER': 'postgres',
+            'PASSWORD': '1234567890',
+            'HOST': 'database-1.czhliuk5ms0u.ap-south-1.rds.amazonaws.com',              
+            'PORT': 5432,                     
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
