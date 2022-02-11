51badd1cdc0885cf4c88f636e1948372477314ff
ashutosh <ashutoshmishra333@gmail.com>
2022-02-11 21:01:44 +0530

    debug

--- live_deployment/settings.py
+++ live_deployment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fjrq(gz!dig57emkps(+!-)df3!t=ul+o3#3lnwuh%if0^l2jp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = 'authentication.User'
 # Application definition
@@ -126,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
