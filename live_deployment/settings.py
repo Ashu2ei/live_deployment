a656935d96d8344007909ec07774d8575ec70669
ashutosh <ashutoshmishra333@gmail.com>
2022-02-12 08:30:41 +0530

    static

--- live_deployment/settings.py
+++ live_deployment/settings.py
@@ -120,10 +120,33 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+
+# aws settings
+AWS_ACCESS_KEY_ID = 'l3wtvDdWtddraE7p+XhSaCNcuNuzfnPB+HazkfRu'
+AWS_SECRET_ACCESS_KEY = 'AKIAQ27O57KF4XLHFXFY'
+AWS_STORAGE_BUCKET_NAME = 'django-static-testing'
+AWS_DEFAULT_ACL = None
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.ap-south-1.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_S3_LOCATION = 'ap-south-1'
+# s3 static settings
+
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# STATIC_URL = '/staticfiles/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+# MEDIA_URL = '/mediafiles/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
